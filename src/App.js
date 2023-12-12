2e218c35ae0c05a7ab50f20bc0c7a1a353f8e115
Your Name <you@example.com>
2023-12-12 19:50:11 +0300

    componenst and pages added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import Skills from './pages/Skills';
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div className="App">
+      <Navbar/>
+     <Routes>
+     <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/skills" element={<Skills />} />
+     </Routes>
+    </div>
   );
 }
-
 export default App;
